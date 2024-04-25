fa941d98f72e8019876855d4eea824bdc8a4e84e
Zzeynepyilmaz <zeynepyilmaz2101@gmail.com>
2024-04-25 14:42:06 +0300

    Initial commit

---
+++ app/src/main/java/com/example/splashscreen/MainViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.splashscreen
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class MainViewModel: ViewModel() {
+    private val _isReady = MutableStateFlow(false)
+    val isReady = _isReady.asStateFlow()
+
+    init{
+        viewModelScope.launch {
+            delay(3000L)
+            _isReady.value = true
+        }
+    }
+}
